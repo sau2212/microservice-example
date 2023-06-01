d1b511d950fd4d8b4d81a33bfb07734aa2bf1598
Saurabh Singh <saurabhsingh2212@gmail.com>
2023-06-01 17:43:21 +0200

    Added twitter keyword related Code

--- twitter-to-kafka-service/src/test/java/com/sau/microservice/twitter/to/kafka/service/TwitterToKafkaServiceApplicationTest.java
+++ twitter-to-kafka-service/src/test/java/com/sau/microservice/twitter/to/kafka/service/TwitterToKafkaServiceApplicationTest.java
@@ -1,11 +1,13 @@
 package com.sau.microservice.twitter.to.kafka.service;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 class TwitterToKafkaServiceApplicationTest {
+
 
     @Test
     public void contextLoad(){
